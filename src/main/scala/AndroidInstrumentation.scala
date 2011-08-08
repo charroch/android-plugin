790afc41723e1c101967194577e7a54e596ec8b3
Carl Harroch <carl@novoda.com>
2011-08-08 07:35:57 +0100

    ...

--- src/main/scala/AndroidInstrumentation.scala
+++ src/main/scala/AndroidInstrumentation.scala
@@ -9,14 +9,13 @@ import AndroidHelpers._
 object AndroidInstrumentation {
 
   private def instrument = (dbPath in Android, manifestSchema in Android, manifestPackage in Android,
-    manifestPath in Android, instrumentationTestRunner in Android, streams) map {
-      (dp, schema, mPackage, amPath, tr, s) ⇒
-        s.log.info("Running instrumentation: " + "shell am instrument -w %s/%s" format (mPackage, tr))
-        adbTask(dp.absolutePath,
-          false,
-          "shell am instrument -w " + mPackage + "/" +
-            tr)
-        ()
+    manifestPath in Android, instrumentationTestRunner in Android, streams) map { (dp, schema, mPackage, amPath, tr, s) ⇒
+      s.log.info("Running instrumentation: " + "shell am instrument -w %s/%s" format (mPackage, tr))
+      adbTask(dp.absolutePath,
+        false,
+        "shell am instrument -w " + mPackage + "/" +
+          tr)
+      ()
     }
 
   lazy val settings: Seq[Setting[_]] = inConfig(Android)(AndroidProject.androidSettings ++ Seq(something <<= { instrument }))
@@ -26,25 +25,28 @@ object AndroidInstrumentation {
   lazy val instrumentationSettings: Seq[Setting[_]] = inConfig(InstrumentationTest)(
     AndroidProject.androidSettings ++ Defaults.testSettings ++ AndroidInstrumentation.settings ++ Defaults.itSettings)
 
-  lazy val defaultAliases = Seq(aaptGenerate, packageDebug)
+  lazy val defaultAliases = Seq(aaptGenerate)
 
-  def coffeeSettings: Seq[Setting[_]] = inConfig(InstrumentationTest)(
-    Defaults.testSettings ++
-      Seq(
-
-        sourceDirectory <<= (baseDirectory in Runtime) { _ / "src" / "it" },
-        resourceDirectory <<= (resourceDirectory in Compile).identity,
-        test <<= test dependsOn (installDevice in InstrumentationTest),
-        resourcesApkName := "resources-test.xap",
-        name := "InstrumentationTest",
-        packageApkName := "teste")
-        ++
-
-        AndroidProject.androidSettings ++ AndroidInstall.settings
-
-        ++ defaultAliases.map(
-          k ⇒ k <<= (k in Android).identity)
-          
-          )
+  def coffeeSettings: Seq[Setting[_]] = inConfig(InstrumentationTest) (Defaults.testSettings ++ AndroidProject.androidSettings ++ AndroidInstall.settings) ++
+  Seq(
+	resourcesApkName <<= (resourcesApkName in Android) {(name) => name + "test"}	  
+  )
+  //      Seq(
+  //
+  //        sourceDirectory <<= (baseDirectory in Runtime) { _ / "src" / "it" },
+  //        resourceDirectory <<= (resourceDirectory in Compile).identity,
+  //        test <<= test dependsOn (installDevice in InstrumentationTest),
+  //        resourcesApkName := "resources-test.xap",
+  //        name := "InstrumentationTest",
+  //        packageDebug <<= (packageDebug in Android) map {(a) => a},
+  //        packageApkName := "teste")
+  //        ++
+  //
+  //        AndroidProject.androidSettings ++ AndroidInstall.settings
+  //
+  //        ++ defaultAliases.map(
+  //          k ⇒ k <<= (k in Android).identity)
+  //          
+  //          )
 
 }
